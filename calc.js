e9e209d5a688966a198e2b6462950a63d06b0bc8
ATarsa <smb909@gmail.com>
2018-02-12 16:38:02 +0000

    works only with two numbers

--- calc.js
+++ calc.js
@@ -14,25 +14,51 @@ function divide(a, b){
     return a / b;
 }
 
-function operate(a,operator, b){
+function equals(a,operator, b){
     if (operator === '+') return add(a,b);
     if (operator === '-') return substract(a,b);
     if (operator === '*') return multiply(a,b);
     if (operator === '/') return divide(a,b);
     }    
-var storedValue = [];
+var storedValue = "";
+var result = 0;
 var display = document.getElementById('display');
 var displayResult = document.getElementById('displayResult');
 var displayOperation = document.getElementById('displayOperation');
 
 var handlers = {
-        displayValue: function(){
-            var targetValue = event.target.value;
-            //storedValue.push(targetValue);
-            displayOperation.innerHTML = targetValue;
-            console.log(targetValue);
-        }
-    };
+    displayValue: function(){
+        var targetValue = event.target.value;
+        storedValue +=targetValue;
+        displayOperation.innerHTML = storedValue;
+        }, 
+    displayOperator: function(){
+        var operator = event.target.value;
+        if (result != 0){
+            displayOperation.innerHTML = result;
+            storedValue = result + " " + operator + " ";
+            displayOperation.innerHTML = storedValue;
+        } 
+        storedValue += " " + operator + " ";
+        displayOperation.innerHTML = storedValue;
+                
+    },
+    operate: function(){
+        var storedValueArray = storedValue.split(" ");
+        console.log(storedValueArray);
+        if (storedValueArray.length === 3){
+            result = equals(storedValueArray[0],storedValueArray[1],storedValueArray[2]);
+            displayResult.innerHTML = result;
+           
+        } 
+        
+        console.log(result);
+        storedValueArray.splice(0,1, result);
+        console.log(storedValueArray);
+        
+        
+    }
+};
 
 // var view = {
 //         displayValue: function(){
