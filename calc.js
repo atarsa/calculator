0171239af239881cd22b7213a2b1eec5c4d53e69
ATarsa <smb909@gmail.com>
2018-02-12 12:59:26 +0000

    feature: pressed number displays on the display

--- calc.js
+++ calc.js
@@ -15,13 +15,37 @@ function divide(a, b){
 }
 
 function operate(a,operator, b){
-    if (operator === '+'){
-        return add(a,b)
-    } else if (operator === '-'){
-        return substract(a,b);
-    } else if (operator === '*'){
-        return multiply(a,b);
-    } else if (operator === '/'){
-        return divide(a,b);
+    if (operator === '+') return add(a,b);
+    if (operator === '-') return substract(a,b);
+    if (operator === '*') return multiply(a,b);
+    if (operator === '/') return divide(a,b);
     }    
-}
+var storedValue = [];
+var display = document.getElementById('display');
+var displayResult = document.getElementById('displayResult');
+var displayOperation = document.getElementById('displayOperation');
+
+var handlers = {
+        displayValue: function(){
+            var targetValue = event.target.value;
+            //storedValue.push(targetValue);
+            displayOperation.innerHTML = targetValue;
+            console.log(targetValue);
+        }
+    };
+
+// var view = {
+//         displayValue: function(){
+//             console.log(event.target.value);
+//         },
+//         setUpEventListeners: function(){
+//             var buttons = document.getElementsByClassName('btn');
+//             var numButtons = buttons.length;
+
+//             for(var i = 0; i < numButtons; i++){
+//                 buttons[i].addEventListener("click",function(event){
+//                     var elementClicked = event.target;
+//                 });
+//             }
+//         }
+//     };
