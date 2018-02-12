36bb8ea8b7fc7a0c72024917f0da0a6e502748ba
ATarsa <smb909@gmail.com>
2018-02-12 20:31:42 +0000

    reorganise handler and view functions

--- calc.js
+++ calc.js
@@ -22,56 +22,71 @@ function equals(a,operator, b){
     }    
 var storedValue = "";
 var result = 0;
-var display = document.getElementById('display');
 var displayResult = document.getElementById('displayResult');
-var displayOperation = document.getElementById('displayOperation');
+
 
 var handlers = {
-    displayValue: function(){
+    getValue: function(){
+        view.getValue();
+    }, 
+    getOperator: function(){
+        view.getOperator();          
+    },
+    operate: function(){
+        view.operate();  
+    },
+    display : function(){
+        view.display();
+    },
+    clear: function(){
+        view.clear();
+    }
+};
+
+var view = {
+    getValue: function(){
         var targetValue = event.target.value;
         storedValue +=targetValue;
-        displayOperation.innerHTML = storedValue;
-        }, 
-    displayOperator: function(){
+        view.display();
+    }, 
+    getOperator: function(){
         var operator = event.target.value;
-        if (result != 0){
-            displayOperation.innerHTML = result;
-            storedValue = result + " " + operator + " ";
-            displayOperation.innerHTML = storedValue;
-        } 
         storedValue += " " + operator + " ";
-        displayOperation.innerHTML = storedValue;
-                
+        view.display();
+               
     },
     operate: function(){
         var storedValueArray = storedValue.split(" ");
         console.log(storedValueArray);
         if (storedValueArray.length === 3){
-            result = equals(storedValueArray[0],storedValueArray[1],storedValueArray[2]);
-            displayResult.innerHTML = result;
-           
-        } 
-        
-        console.log(result);
-        storedValueArray.splice(0,1, result);
-        console.log(storedValueArray);
-        
-        
+            result = equals(parseFloat(storedValueArray[0]),storedValueArray[1],parseFloat(storedValueArray[2]));
+            storedValue = result;
+            view.display();
+       
+        }
+               
+    },
+    display : function(){
+        if (storedValue === ""){
+            displayResult.innerHTML = 0; 
+        } else {
+            displayResult.innerHTML = storedValue;
+            
+        }
+    },
+    clear: function(){
+        storedValue = "";
+        displayResult.innerHTML = 0;
+    },    
+    setUpEventListeners: function(){
+        var buttons = document.getElementsByClassName('btn');
+        var numButtons = buttons.length;
+
+        for(var i = 0; i < numButtons; i++){
+            buttons[i].addEventListener("click",function(event){
+                var elementClicked = event.target;
+                return elementClicked;
+            });
+        }
     }
 };
-
-// var view = {
-//         displayValue: function(){
-//             console.log(event.target.value);
-//         },
-//         setUpEventListeners: function(){
-//             var buttons = document.getElementsByClassName('btn');
-//             var numButtons = buttons.length;
-
-//             for(var i = 0; i < numButtons; i++){
-//                 buttons[i].addEventListener("click",function(event){
-//                     var elementClicked = event.target;
-//                 });
-//             }
-//         }
-//     };
