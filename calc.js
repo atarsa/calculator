82eb53ae1888b305363744e04c7bb2e2682c4678
ATarsa <smb909@gmail.com>
2018-02-13 18:43:00 +0000

    Added basic functionality

--- calc.js
+++ calc.js
@@ -1,92 +1,68 @@
-function add(a, b){
-    return a + b;
-};
+// Get the keys from document
+var keys = document.querySelectorAll('[type="button"]');
+var operators = ['+','-','x','÷'];
+var decimalAdded = false;
 
-function substract(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
-}
-
-function equals(a,operator, b){
-    if (operator === '+') return add(a,b);
-    if (operator === '-') return substract(a,b);
-    if (operator === '*') return multiply(a,b);
-    if (operator === '/') return divide(a,b);
-    }    
-var storedValue = "";
-var result = 0;
-var displayResult = document.getElementById('displayResult');
-
-
-var handlers = {
-    getValue: function(){
-        view.getValue();
-    }, 
-    getOperator: function(){
-        view.getOperator();          
-    },
-    operate: function(){
-        view.operate();  
-    },
-    display : function(){
-        view.display();
-    },
-    clear: function(){
-        view.clear();
-    }
-};
-
-var view = {
-    getValue: function(){
-        var targetValue = event.target.value;
-        storedValue +=targetValue;
-        view.display();
-    }, 
-    getOperator: function(){
-        var operator = event.target.value;
-        storedValue += " " + operator + " ";
-        view.display();
-               
-    },
-    operate: function(){
-        var storedValueArray = storedValue.split(" ");
-        console.log(storedValueArray);
-        if (storedValueArray.length === 3){
-            result = equals(parseFloat(storedValueArray[0]),storedValueArray[1],parseFloat(storedValueArray[2]));
-            storedValue = result;
-            view.display();
-       
+for( var i = 0; i < keys.length; i++){
+    keys[i].onclick = function(e){
+        var display = document.querySelector('#displayResult');
+        var inputValue = display.innerHTML;
+        var btnValue = this.value;
+        console.log(btnValue);
+      
+        // if clear key is pressed, erase everything
+        if (btnValue === "C"){
+            display.innerHTML = "";
+            decimalAdded = false;
         }
-               
-    },
-    display : function(){
-        if (storedValue === ""){
-            displayResult.innerHTML = 0; 
-        } else {
-            displayResult.innerHTML = storedValue;
+
+        // if eval key is pressed, calculate and display the result; use eval()
+        else if (btnValue === "="){
             
-        }
-    },
-    clear: function(){
-        storedValue = "";
-        displayResult.innerHTML = 0;
-    },    
-    setUpEventListeners: function(){
-        var buttons = document.getElementsByClassName('btn');
-        var numButtons = buttons.length;
+            var equation = inputValue;
+            var lastChar = equation[equation.length-1];
+            // replace all instances of x and ÷ with * and /; use regex expressions
+            equation = equation.replace(/x/g,'*').replace(/÷/g,'/');
 
-        for(var i = 0; i < numButtons; i++){
-            buttons[i].addEventListener("click",function(event){
-                var elementClicked = event.target;
-                return elementClicked;
-            });
+            // check the last character of equation; if an operator or a decimal remove it
+            if (operators.lastIndexOf(lastChar) > -1 || lastChar === "." ){
+                equation = equation.replace(/.$/,''); 
+            }
+            if(equation) 
+                display.innerHTML = eval(equation);
+            
+            decimalAdded = false;    
+        }
+        // No more than one operator should be added
+        else if (operators.indexOf(btnValue) > -1){
+            // Operator is clicked
+            // Get the last character from equation
+            var lastChar = inputValue[inputValue.length-1];
+
+            // Only add operator if input is not empty and there is so operator on the last char
+            if(inputValue != "" && operators.indexOf(lastChar) === -1)
+                display.innerHTML += btnValue;
+            
+            // Allow minus sign as first operator
+            else if (inputValue === "" && btnValue === "-")
+                display.innerHTML += btnValue;
+            // Replace the last operator (if exists) with newly passed operator
+            if (operators.indexOf(lastChar) > -1 && inputValue.length > 1)
+                display.innerHTML = inputValue.replace(/.$/,btnValue)
+
+            decimalAdded = false;    
+        }   
+        
+        // Prevent pressing more than one decimal point
+        else if (btnValue === '.'){
+            if (decimalAdded === false){
+                display.innerHTML += btnValue;
+                decimalAdded = true;
+            }
+        }
+        // if any other key pressed - append it
+        else {
+            display.innerHTML += btnValue;
         }
     }
-};
+}
